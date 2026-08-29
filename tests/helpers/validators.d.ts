1d2efcbc9e6262a05a4e858137701ac49acc6b0b
Äppi <80828283+Appstun@users.noreply.github.com>
2026-08-29 23:49:28 +0200

    Removed test scripts

--- tests/helpers/validators.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function assertTwitchStreamData(data: Record<string, unknown>): void;
-export declare function assertTwitchVod(data: Record<string, unknown>): void;
-export declare function assertScheduleEntry(entry: Record<string, unknown>): void;
-export declare function assertScheduleResponse(data: Record<string, unknown>): void;
-export declare function assertScheduleLatestResponse(data: Record<string, unknown>): void;
-export declare function assertSubathonGoal(goal: Record<string, unknown>): void;
-export declare function assertSubathonData(data: Record<string, unknown>): void;
-export declare function assertBlogFeedData(data: Record<string, unknown>, expectRaw?: boolean): void;
-export declare function assertXFeedData(data: Record<string, unknown>, expectRaw?: boolean): void;
